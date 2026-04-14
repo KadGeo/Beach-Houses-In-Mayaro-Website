fddf304ae1baca57f5c371ec6290db7b52a80520
Kaege Foreman <kaegforeman@gmai.com  git config --global user.name Kaege>
2026-04-14 16:43:30 -0400

    google pagespeed insight adjustment made conveyor manual with arrow

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@
     window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
   }
 
-  document.addEventListener('scroll', toggleScrolled);
+  document.addEventListener('scroll', toggleScrolled, { passive: true });
   window.addEventListener('load', toggleScrolled);
 
   /**
@@ -89,7 +89,7 @@
   });
 
   window.addEventListener('load', toggleScrollTop);
-  document.addEventListener('scroll', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop, { passive: true });
 
   /**
    * Animation on scroll function and init
