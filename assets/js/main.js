369f3aa85b7aad4f89c81d1afacb5d67db9f2c41
Kaege Foreman <kaegforeman@gmai.com  git config --global user.name Kaege>
2026-04-14 17:06:22 -0400

    google pagespeed adjustment 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,10 @@
   /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
+  const selectBody = document.querySelector('body');
+  const selectHeader = document.querySelector('#header');
+
   function toggleScrolled() {
-    const selectBody = document.querySelector('body');
-    const selectHeader = document.querySelector('#header');
     if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
     window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
   }
